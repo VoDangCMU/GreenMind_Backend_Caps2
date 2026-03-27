e692b6d110d8ba3a2a611c7c7993bcc5609fffaf
Tran Dinh Quan <trandinhquan.1803@gmail.com>
2026-03-28 00:00:23 +0700

    refactor: WasteReportRoutes

--- src/routes/wasteReportRoutes.ts
+++ src/routes/wasteReportRoutes.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import { jwtAuthMiddleware } from '../middlewares/jwtMiddleware';
 import wasteReportController from '../controller/wasteReportController';
-import { adminMiddleware } from '../middlewares/adminMiddleware';
 
 const router = Router();
 
