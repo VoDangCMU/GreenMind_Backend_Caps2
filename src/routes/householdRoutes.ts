057d80af7b4e266ec4592fef02f18a8ea4250397
hotanphong <phongho763@gmail.com>
2026-04-08 17:56:23 +0700

    add get hh detect by type and check deploy

--- src/routes/householdRoutes.ts
+++ src/routes/householdRoutes.ts
@@ -15,6 +15,7 @@ router.post("/detect-trash", DetectTrashController.DetectTrashOnly);
 router.post("/predict-pollutant", DetectTrashController.PredictPollutantImpact);
 router.post("/total-mass", DetectTrashController.TotalMass);
 router.get("/detect-trash/:type", DetectTrashController.getDetectionByType);
+router.get("/detect-trash/historyByHousehold/:type", DetectTrashController.getDetectionByTypeHousehold);
 router.get("/detect-trash/historyByUser", DetectTrashController.getDetectionHistoryByUser);
 router.get("/detect-trash/historyByHousehold", DetectTrashController.getDetectionHistoryByHousehold);
 router.get("/get-detect-by-household/:id", DetectTrashController.getHouseholdById);
