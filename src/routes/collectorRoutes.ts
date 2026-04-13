92786638d9573a89c467c9defdc7007c8c8f7a63
hotanphong <phongho763@gmail.com>
2026-04-13 14:57:40 +0700

    add 4 api

--- src/routes/collectorRoutes.ts
+++ src/routes/collectorRoutes.ts
@@ -7,5 +7,6 @@ router.use(jwtAuthMiddleware);
 
 router.get("/get-brought-out", DetectTrashController.getPendingPickups);
 router.post("/pickups/:id/checkin", DetectTrashController.pickupWaste);
-
+router.get("/historyByUser", DetectTrashController.collectorHistory);
+router.get("/all-picked-ups", DetectTrashController.getAllPickedUp);
 export default router;
