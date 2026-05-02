e815d9efbc5bd4d78bf285e5fdc6022be9b7c298
Tran Dinh Quan <trandinhquan.1803@gmail.com>
2026-05-02 23:32:29 +0700

    feat: chat list campaign

--- src/routes/campaignRoutes.ts
+++ src/routes/campaignRoutes.ts
@@ -6,6 +6,7 @@ const router = Router();
 
 router.post('/', jwtAuthMiddleware, campaignController.createCampaign);
 router.get('/', jwtAuthMiddleware, campaignController.getAllCampaigns);
+router.get('/chat-list', jwtAuthMiddleware, campaignController.getUserChatList);
 router.get('/:id', jwtAuthMiddleware, campaignController.getCampaignById);
 router.put('/:id', jwtAuthMiddleware, campaignController.updateCampaign);
 router.delete('/:id', jwtAuthMiddleware, campaignController.deleteCampaign);
